4968d1a79fc8f52afc0632eefa7d308ca7c2dc77
MarySummer24 <90946266+MarySummer24@users.noreply.github.com>
2023-06-14 20:34:45 -0600

    Update ALexico.java

--- Analizador4/src/AnalizadorL/ALexico.java
+++ Analizador4/src/AnalizadorL/ALexico.java
@@ -7,7 +7,7 @@ public class ALexico
 	public static void main(String[] args)
 	{
 		//Cambiar ruta para cada compu
-		String ruta="D:/users/alelo/eclipse-workspace/Analizador4/src/AnalizadorL/Lexer.flex";
+		String ruta="C:/users/MarySummer/eclipse-workspace/Analizador4/src/AnalizadorL/Lexer.flex";
 		//URL ruta = getClass().getResource("/AnalizadorL/Lexer.flex");
 		generateLexer(ruta);
 	}
